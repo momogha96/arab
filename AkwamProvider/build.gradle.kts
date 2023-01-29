b8b25be71c355d12d317f9a39dad68f94191d304
momogha96 <121405653+momogha96@users.noreply.github.com>
2023-01-29 20:58:56 +0200

    Update build.gradle.kts

--- AkwamProvider/build.gradle.kts
+++ AkwamProvider/build.gradle.kts
@@ -8,7 +8,7 @@ cloudstream {
 	
     status = 1
 
-    tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "Cartoon" )
+    tvTypes = listOf( "TvSeries" , "Movie" , "movies?section=0&category=0&rating=0&year=0&language=1&formats=0&quality=0" , "Anime" , "Cartoon" )
 
     iconUrl = "https://www.google.com/s2/favicons?domain=akwam.to&sz=%size%"
 }
